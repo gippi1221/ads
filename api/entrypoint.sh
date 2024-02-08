379c5f8536f7d28d19f1949599befd4384c0f755
gennadii garshin <gippi122221@gmail.com>
2024-02-08 18:50:15 +0100

    kafka validation

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -19,6 +19,26 @@ print("ClickHouse is ready!")
 END
 }
 
+check_kafka() {
+    python3 - <<END
+import socket
+import time
+
+def is_kafka_available():
+    try:
+        socket.create_connection(("kafka", 9092), timeout=5)
+        return True
+    except (socket.timeout, ConnectionRefusedError):
+        return False
+
+print("Waiting for Kafka to start...")
+while not is_kafka_available():
+    time.sleep(1)
+print("Kafka is ready!")
+END
+}
+
 check_clickhouse
+check_kafka
 
 gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app -b 0.0.0.0:8000
